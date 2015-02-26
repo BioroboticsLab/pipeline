e236f5ae73fd79c79d2d39680e10c1a137dd626f
Tobias Ludwig <tludwig@inf.fu-berlin.de>
2015-02-26 11:42:44 +0100

    comment, clean up, ...

--- fill_poly_main.cpp
+++ fill_poly_main.cpp
@@ -6,7 +6,7 @@
  */
 
 
-
+#include "helper.h"
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <algorithm>
@@ -44,6 +44,9 @@ template<>
 inline cv::Scalar default_color<CV_32FC3>() { return {1.0f, 0.0f, 0.2f}; }
 
 
+/**
+ * RAII timer
+ */
 class timer {
 private:
 	const std::chrono::system_clock::time_point start;
@@ -60,6 +63,9 @@ public:
 
 /**
  * compares cv::fillConvexPoly & heyho::fillConvexPoly
+ *
+ * - displays diffrences
+ *
  */
 struct compare {
 
@@ -67,7 +73,8 @@ struct compare {
 	const int angle;
 
 	template<typename pixel_t, int line_type>
-	bool operator()() const {
+	bool operator()() const
+	{
 		constexpr int img_type = cv::DataType<pixel_t>::type;
 		constexpr int shift = 0;
 		const int dim = 2 * std::max(axes.width, axes.height) + 10;
@@ -86,7 +93,6 @@ struct compare {
 		const bool equal =  0 == std::memcmp(img1.datastart, img2.datastart, img1.dataend - img1.datastart);
 
 		if (! equal) {
-
 			cv::namedWindow( "opencv", cv::WINDOW_AUTOSIZE);
 			cv::imshow( "opencv", img1);
 
@@ -99,24 +105,24 @@ struct compare {
 			cv::imshow( "diff", diff);
 
 			cv::waitKey(0);
-
-
 		}
-
 		return equal;
 	}
 };
 
 
+/**
+ * benchmarks cv::fillConvexPoly & heyho::fillConvexPoly
+ */
 struct benchmark {
-
 
 	const cv::Size axes;
 	const int angle;
 	std::size_t times;
 
 	template<typename pixel_t, int line_type>
-	bool operator()() const {
+	bool operator()() const
+	{
 		constexpr int img_type = cv::DataType<pixel_t>::type;
 		constexpr int shift = 0;
 		const int dim = 2 * std::max(axes.width, axes.height) + 10;
@@ -124,15 +130,17 @@ struct benchmark {
 		std::vector<cv::Point> points;
 		cv::ellipse2Poly(center, axes, angle, 0, 360, 1, points);
 		cv::Mat img(dim, dim, img_type, white<img_type>());
+
+		std::cout << "img type: " << img_type_to_str(img_type) << " line type: " << line_type << '\n';
 		{
-			std::cout << "opencv: ";
+			std::cout << "    opencv: ";
 			timer t;
 			for (size_t i = 0; i < times; ++i) {
 				cv::fillConvexPoly(img, points, default_color<img_type>(), line_type, shift);
 			}
 		}
 		{
-			std::cout << "heyho:  ";
+			std::cout << "    heyho:  ";
 			timer t;
 			for (size_t i = 0; i < times; ++i) {
 				heyho::fill_convex_poly<pixel_t>(img, points, default_color<img_type>(), line_type);
@@ -146,6 +154,9 @@ struct benchmark {
 
 /**
  * calls f with each combination of CV image type & CV line type
+ *
+ * @throws iff f returns false
+ *
  */
 struct foreach {
 
@@ -175,17 +186,18 @@ struct foreach {
 
 };
 
-
-const cv::Scalar BLUE(255,    0,   0);
-const cv::Scalar WHITE(255, 255, 255);
-
 using fill_convex_poly_f = void (*) (cv::InputOutputArray img, cv::InputArray points, const cv::Scalar& color, int line_type);
 
 inline void cv_fill_confex_poly(cv::InputOutputArray img, cv::InputArray points, const cv::Scalar& color, int line_type) {
 	cv::fillConvexPoly(img, points, color, line_type);
 }
 
-void bench_fill_convex_poly(const std::vector<fill_convex_poly_f> &e, int major, size_t times) {
+/**
+ * benchmarks multiple fill convex poly functions
+ */
+void benchmark_fill_convex_poly_functions(const std::vector<fill_convex_poly_f> &fill_functions, int major, size_t times)
+{
+	constexpr int img_type = CV_8UC1;
 	const int axis_major = major;
 	const int axis_minor = axis_major / 2;
 	const int dim_y = 2 * std::max(axis_major, axis_minor) + 10;
@@ -193,41 +205,47 @@ void bench_fill_convex_poly(const std::vector<fill_convex_poly_f> &e, int major,
 	const cv::Point center(dim_x/2, dim_y/2);
 	std::vector<cv::Point> poly;
 	cv::ellipse2Poly(center, cv::Size(axis_major, axis_minor), 45, 0, 360, 1, poly);
-	cv::Mat img(dim_y, dim_x, CV_8UC3, WHITE);
+	cv::Mat img(dim_y, dim_x, img_type, white<img_type>());
 
-	for (auto f : e) {
+	std::cout << "benchmark multiple functions (" << img_type_to_str(img.type()) << ") :\n";
+
+	for (size_t i = 0; i < fill_functions.size(); ++i) {
+		std::cout << "  function "<< std::setw(2) << i << ": ";
+		std::cout.flush();
 		timer t;
-		for (size_t i = 0; i < times; ++i) {
-			f(img, poly, BLUE, 4);
+		for (size_t j = 0; j < times; ++j) {
+			fill_functions[i](img, poly, default_color<img_type>(), 4);
 		}
 	}
 }
 
 int main() {
-
-	for (int a = 0; a < 45; a += 5) {
-		for (int a1 = 25; a1 < 50; ++a1) {
-			for (int a2 = a1; a2 < 50; ++a2) {
-				foreach()(compare{{a1, a2}, a});
+	{
+		for (int a = 0; a < 45; a += 5) {
+			for (int a1 = 25; a1 < 50; ++a1) {
+				for (int a2 = a1; a2 < 50; ++a2) {
+					foreach()(compare{{a1, a2}, a});
+				}
 			}
 		}
+		std::cout << "FOREACH( cv::fillConvexPoly == heyho::fill_convex_poly ) : passed :)\n\n";
 	}
-	std::cout << "FOREACH: passed :)\n";
 
+	if (false or true)
+	{
+		const std::vector<fill_convex_poly_f> fill_functions {
+			&cv_fill_confex_poly,
+			static_cast<fill_convex_poly_f>(&heyho::fill_convex_poly<uint8_t>)
+		};
+		benchmark_fill_convex_poly_functions(fill_functions, 400, 5000);
+		std::cout << '\n';
+	}
 
-
-
-
-	std::vector<fill_convex_poly_f> e{
-		&cv_fill_confex_poly,
-		static_cast<fill_convex_poly_f>(&heyho::fill_convex_poly<cv::Vec<uint8_t, 3>>)
-	};
-
-	if (false or true) {
-
+	if (false or true)
+	{
+		std::cout << "BENCHMARK\n==================\n";
 		foreach()(benchmark{{400, 200}, 45, 500});
-
-		bench_fill_convex_poly(e, 400, 5000);
+		std::cout << '\n';
 	}
 
 	return 0;
