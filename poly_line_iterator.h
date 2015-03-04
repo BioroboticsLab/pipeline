3eb8cf582064b144b2955676b900b9edb807a82c
Tobias Ludwig <tludwig@inf.fu-berlin.de>
2015-03-04 14:26:00 +0100

    add poly line iterator

---
+++ poly_line_iterator.h
@@ -0,0 +1,57 @@
+/*
+ * poly_line_iterator.h
+ *
+ *  Created on: Mar 2, 2015
+ *      Author: tobias
+ */
+
+#ifndef POLY_LINE_ITERATOR_H_
+#define POLY_LINE_ITERATOR_H_
+
+#include "ring_iterator.h"    // heyho::ring_iterator
+#include "line_iterator.h"    // heyho::line_iterator
+#include <opencv2/opencv.hpp> // cv::Point
+#include <type_traits>        // std::is_same
+
+namespace heyho {
+
+	/**
+	 * iterates over the points of the lines connecting the ring_iterator's points
+	 *
+	 * @tparam POINTS_IT required operations:
+	 *                     POINTS_IT& POINTS_IT::operator++()
+	 *                     bool POINTS_IT::end()
+	 *                     typename POINTS_IT::value_type
+	 *                     POINTS_IT::value_type POINTS_IT::operator*();
+	 */
+	template<typename POINTS_IT>
+	class poly_lines_iterator {
+	public:
+		using value_type = cv::Point;
+
+		static_assert(std::is_same<typename POINTS_IT::value_type, value_type>::value, "invalid point type");
+
+		explicit poly_lines_iterator(POINTS_IT points_it, int connectivity);
+
+		bool end() const;
+
+		value_type operator*() const;
+
+		poly_lines_iterator& operator++();
+
+	private:
+		static heyho::line_iterator make_line_it(POINTS_IT &it, int connectivity);
+
+		POINTS_IT m_points;
+		heyho::line_iterator m_current_line;
+	};
+
+	namespace tests {
+		void poly_line_iterator_tests();
+	}
+
+}
+
+#include "poly_line_iterator_impl.h"
+
+#endif /* POLY_LINE_ITERATOR_H_ */
---
+++ poly_line_iterator_impl.h
@@ -0,0 +1,66 @@
+/*
+ * poly_line_iterator_impl.h
+ *
+ *  Created on: Mar 4, 2015
+ *      Author: tobias
+ */
+
+#ifndef POLY_LINE_ITERATOR_IMPL_H_
+#define POLY_LINE_ITERATOR_IMPL_H_
+
+namespace heyho {
+
+	template<typename POINTS_IT>
+	inline poly_lines_iterator<POINTS_IT>::poly_lines_iterator(POINTS_IT points_it, int connectivity)
+		: m_points(std::move(points_it))
+		, m_current_line(make_line_it(m_points, connectivity))
+	{}
+
+	template<typename POINTS_IT>
+	inline bool poly_lines_iterator<POINTS_IT>::end() const {
+		return m_points.end() && m_current_line.end();
+	}
+
+	template<typename POINTS_IT>
+	inline typename poly_lines_iterator<POINTS_IT>::value_type poly_lines_iterator<POINTS_IT>::operator*() const {
+		if (this->end()) {
+			throw std::runtime_error("dereferencing invalid iterator");
+		}
+		return *m_current_line;
+	}
+
+	template<typename POINTS_IT>
+	inline poly_lines_iterator<POINTS_IT>& poly_lines_iterator<POINTS_IT>::operator++()
+	{
+		if (!this->end())
+		{
+			// advance to next pixel
+			++m_current_line;
+
+			// end of current line is reached --> advance to next line
+			// (this is a loop instead of an if, in order to skip single-pixel lines)
+			while (m_current_line.end() && !m_points.end())
+			{
+				const auto prev_line_point = *m_points;
+				++m_points;
+				if (!m_points.end()) {
+					m_current_line = line_iterator(prev_line_point, *m_points, m_current_line.connectivity());
+					// this isn't the first line --> skipt it's first pixel == previous line's last pixel
+					++m_current_line;
+				}
+			}
+		}
+		return *this;
+	}
+
+	template<typename POINTS_IT>
+	inline heyho::line_iterator poly_lines_iterator<POINTS_IT>::make_line_it(POINTS_IT &it, int connectivity) {
+		const auto p1 = *it;
+		++it;
+		const auto p2 = it.end() ? p1 : *it;
+		return heyho::line_iterator(p1, p2, connectivity);
+	}
+
+}
+
+#endif /* POLY_LINE_ITERATOR_IMPL_H_ */
