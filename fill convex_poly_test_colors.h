a65c1a07a1350feabcd2578ffdfb83de7709beba
Tobias Ludwig <tludwig@inf.fu-berlin.de>
2015-03-12 17:02:14 +0100

    colors : add black

--- fill convex_poly_test_colors.h
+++ fill convex_poly_test_colors.h
@@ -26,6 +26,30 @@ namespace heyho {
 			template<>
 			inline cv::Scalar white<CV_32FC3>() { return {1.0f, 1.0f, 1.0f}; }
 
+			template<typename pixel_t>
+			inline cv::Scalar pixel_2_white() {
+				return white<cv::DataType<pixel_t>::type>();
+			}
+
+
+			template<int>
+			cv::Scalar black();
+			template<>
+			inline cv::Scalar black<CV_8UC1>() { return {0}; }
+			template<>
+			inline cv::Scalar black<CV_8SC1>() { return {-128}; }
+			template<>
+			inline cv::Scalar black<CV_8UC3>() { return {0, 0, 0}; }
+			template<>
+			inline cv::Scalar black<CV_32FC1>() { return {0.0f}; }
+			template<>
+			inline cv::Scalar black<CV_32FC3>() { return {0.0f, 0.0f, 0.0f}; }
+
+			template<typename pixel_t>
+			inline cv::Scalar pixel_2_black() {
+				return black<cv::DataType<pixel_t>::type>();
+			}
+
 
 			template<int>
 			cv::Scalar default_color();
@@ -39,6 +63,11 @@ namespace heyho {
 			inline cv::Scalar default_color<CV_32FC1>() { return {0.4f}; }
 			template<>
 			inline cv::Scalar default_color<CV_32FC3>() { return {1.0f, 0.0f, 0.2f}; }
+
+			template<typename pixel_t>
+			inline cv::Scalar pixel_2_default() {
+				return default_color<cv::DataType<pixel_t>::type>();
+			}
 	}
 
 }
