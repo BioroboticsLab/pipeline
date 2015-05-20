44ebdf6d6cf03035db2eaebdd38052780629d1f6
Benjamin Wild <b.w@fu-berlin.de>
2015-05-20 18:18:12 +0200

    add print() function to settings

--- datastructure/settings.h
+++ datastructure/settings.h
@@ -74,6 +74,17 @@ public:
 
 class settings_abs {
 
+private:
+	void print(boost::property_tree::ptree const& pt)
+	{
+		using boost::property_tree::ptree;
+		ptree::const_iterator end = pt.end();
+		for (ptree::const_iterator it = pt.begin(); it != end; ++it) {
+			std::cout << it->first << ": " << it->second.get_value<std::string>() << std::endl;
+			print(it->second);
+		}
+	}
+
 protected:
 	std::map<std::string, setting_entry> _settings;
 	std::string _base;
@@ -150,6 +161,11 @@ public:
 		boost::property_tree::ptree pt;
 		addToPTree(pt);
 		return pt;
+	}
+
+	void print()
+	{
+		print(getPTree());
 	}
 
 	/**
