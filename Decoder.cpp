5e311bfce375f2ef2736b33b9b4f5698d59f634e
Benjamin Wild <b.w@fu-berlin.de>
2015-07-31 16:39:57 +0200

    uniform indentation for decoder

--- Decoder.cpp
+++ Decoder.cpp
@@ -19,38 +19,38 @@ Decoder::Decoder()
 
 void Decoder::loadSettings(decoder_settings_t &&settings)
 {
-	_settings = std::move(settings);
+    _settings = std::move(settings);
 }
 
 std::vector<Tag> Decoder::process(std::vector<Tag> &&taglist)
 {
 #ifdef DEBUG_DECODER
-	for (Tag& tag : taglist) {
-		for (TagCandidate& candidate : tag.getCandidates()) {
-			std::vector<decoding_t> decodings = getDecodings(tag, candidate);
-			candidate.setDecodings(std::move(decodings));
-		}
-	}
+    for (Tag& tag : taglist) {
+        for (TagCandidate& candidate : tag.getCandidates()) {
+            std::vector<decoding_t> decodings = getDecodings(tag, candidate);
+            candidate.setDecodings(std::move(decodings));
+        }
+    }
 #else
-	static const size_t numThreads = std::thread::hardware_concurrency() ?
-	            std::thread::hardware_concurrency() * 2 : 1;
-	ThreadPool pool(numThreads);
+    static const size_t numThreads = std::thread::hardware_concurrency() ?
+                std::thread::hardware_concurrency() * 2 : 1;
+    ThreadPool pool(numThreads);
 
-	std::vector<std::future<void>> results;
-	for (Tag& tag : taglist) {
-		results.emplace_back(
-		            pool.enqueue([&] {
-			for (TagCandidate& candidate : tag.getCandidates()) {
-				std::vector<decoding_t> decodings = getDecodings(tag, candidate);
-				candidate.setDecodings(std::move(decodings));
-			}
-		}));
-	}
+    std::vector<std::future<void>> results;
+    for (Tag& tag : taglist) {
+        results.emplace_back(
+                    pool.enqueue([&] {
+            for (TagCandidate& candidate : tag.getCandidates()) {
+                std::vector<decoding_t> decodings = getDecodings(tag, candidate);
+                candidate.setDecodings(std::move(decodings));
+            }
+        }));
+    }
 
-	for (auto && result : results) result.get();
+    for (auto && result : results) result.get();
 #endif
 
-	return std::move(taglist);
+    return std::move(taglist);
 }
 
 std::vector<decoding_t> Decoder::getDecodings(const Tag &tag, TagCandidate &candidate) const
@@ -58,39 +58,39 @@ std::vector<decoding_t> Decoder::getDecodings(const Tag &tag, TagCandidate &cand
     const double BLUR_KERNEL [3] = {0.25,0.50,0.25};
     const double SHARPENING_FACTOR = 2.0;
 
-	// region of interest of tag candidate
+    // region of interest of tag candidate
     const cv::Mat& roi = tag.getOrigSubImage();
 
-	const cv::Point roiOffset = tag.getBox().tl();
+    const cv::Point roiOffset = tag.getBox().tl();
 
-	std::vector<decoding_t> decodings;
-	for (PipelineGrid& grid : candidate.getGrids()) {
+    std::vector<decoding_t> decodings;
+    for (PipelineGrid& grid : candidate.getGrids()) {
 
         decoding_t decoding;
         double cellMedians[Grid::NUM_MIDDLE_CELLS];
         double blurredCellMedians[Grid::NUM_MIDDLE_CELLS];
         double sharpenedCellMedians[Grid::NUM_MIDDLE_CELLS];
 
-		mean_calculator_t blackMeanCalculator(roi, roiOffset);
-		blackMeanCalculator = grid.processInnerBlackRingCoordinates(std::move(blackMeanCalculator));
+        mean_calculator_t blackMeanCalculator(roi, roiOffset);
+        blackMeanCalculator = grid.processInnerBlackRingCoordinates(std::move(blackMeanCalculator));
 
-		mean_calculator_t whiteMeanCalculator(roi, roiOffset);
-		whiteMeanCalculator = grid.processInnerWhiteRingCoordinates(std::move(whiteMeanCalculator));
+        mean_calculator_t whiteMeanCalculator(roi, roiOffset);
+        whiteMeanCalculator = grid.processInnerWhiteRingCoordinates(std::move(whiteMeanCalculator));
 
-		// grid cell coordinates have to be initialized before outer ring coordinates. only small
-		// performance overhead because they will be cached by the PipelineGrid for the next use
-		for (size_t idx = 0; idx < Grid::NUM_MIDDLE_CELLS; ++ idx) {
-			// just calculate the coordinates, do nothing with them
-			grid.processGridCellCoordinates(idx, dummy_functor_t());
-		}
+        // grid cell coordinates have to be initialized before outer ring coordinates. only small
+        // performance overhead because they will be cached by the PipelineGrid for the next use
+        for (size_t idx = 0; idx < Grid::NUM_MIDDLE_CELLS; ++ idx) {
+            // just calculate the coordinates, do nothing with them
+            grid.processGridCellCoordinates(idx, dummy_functor_t());
+        }
         whiteMeanCalculator = grid.processOuterRingCoordinates(std::move(whiteMeanCalculator));
         //grid.processOuterRingCoordinates(dummy_functor_t());
 
-		const double meanBlack = blackMeanCalculator.getMean();
-		const double meanWhite = whiteMeanCalculator.getMean();
+        const double meanBlack = blackMeanCalculator.getMean();
+        const double meanWhite = whiteMeanCalculator.getMean();
         const double bwSpread = std::abs(meanWhite - meanBlack);
 
-		for (size_t idx = 0; idx < Grid::NUM_MIDDLE_CELLS; ++ idx) {
+        for (size_t idx = 0; idx < Grid::NUM_MIDDLE_CELLS; ++ idx) {
 
             mean_calculator_t gridCellMeanCalculator(roi, roiOffset);
             gridCellMeanCalculator = grid.processGridCellCoordinates(idx, std::move(gridCellMeanCalculator));
@@ -121,7 +121,7 @@ std::vector<decoding_t> Decoder::getDecodings(const Tag &tag, TagCandidate &cand
                 decoding.set(idx, true);
 
             }
-		}
+        }
 
         for (size_t idx = 0; idx < Grid::NUM_MIDDLE_CELLS; ++ idx) {
 
@@ -213,7 +213,7 @@ std::vector<decoding_t> Decoder::getDecodings(const Tag &tag, TagCandidate &cand
             std::cout << decoding[(i)%Grid::NUM_MIDDLE_CELLS];
         }
         std::cout << "\n";
-/*
+        /*
         std::cout << "Center: " << grid.getCenter() << "; radius: " << grid.getRadius() << "\n";
         std::cout << "Outer ring coords:\n";
 */
@@ -224,72 +224,72 @@ std::vector<decoding_t> Decoder::getDecodings(const Tag &tag, TagCandidate &cand
         visualizeDebug(tag, grid, decoding);
 #endif
 
-		decodings.push_back(decoding);
-	}
-	return decodings;
+        decodings.push_back(decoding);
+    }
+    return decodings;
 }
 
 void Decoder::visualizeDebug(const Tag &tag, PipelineGrid &grid, pipeline::decoding_t const& decoding) const
 {
-	grid.setCenter(grid.getCenter() - tag.getBox().tl());
+    grid.setCenter(grid.getCenter() - tag.getBox().tl());
 
-	cv::Mat roi = tag.getOrigSubImage();
-	cv::Size roiSize = roi.size();
+    cv::Mat roi = tag.getOrigSubImage();
+    cv::Size roiSize = roi.size();
 
-	std::vector<cv::Mat> images;
+    std::vector<cv::Mat> images;
 
-	images.push_back(tag.getOrigSubImage());
+    images.push_back(tag.getOrigSubImage());
 
-	images.push_back(grid.getProjectedImage(roiSize));
+    images.push_back(grid.getProjectedImage(roiSize));
 
-	cv::Mat blended;
-	cv::addWeighted(tag.getOrigSubImage(), 0.8, grid.getProjectedImage(roiSize), 0.2, 0.0, blended);
-	images.push_back(blended);
+    cv::Mat blended;
+    cv::addWeighted(tag.getOrigSubImage(), 0.8, grid.getProjectedImage(roiSize), 0.2, 0.0, blended);
+    images.push_back(blended);
 
-	cv::Mat origCopyOverlay;
-	tag.getOrigSubImage().copyTo(origCopyOverlay);
-	grid.drawContours(origCopyOverlay, 0.5);
-	images.push_back(origCopyOverlay);
+    cv::Mat origCopyOverlay;
+    tag.getOrigSubImage().copyTo(origCopyOverlay);
+    grid.drawContours(origCopyOverlay, 0.5);
+    images.push_back(origCopyOverlay);
 
-	cv::Mat test(roiSize, CV_8UC1, cv::Scalar::all(0));
-	grid.draw(test, boost::make_optional(decoding));
+    cv::Mat test(roiSize, CV_8UC1, cv::Scalar::all(0));
+    grid.draw(test, boost::make_optional(decoding));
     images.push_back(test.clone());
 
-	const auto canvas = CvHelper::makeCanvas(images, images[0].rows + 10, 1);
+    const auto canvas = CvHelper::makeCanvas(images, images[0].rows + 10, 1);
 
-	grid.setCenter(grid.getCenter() + tag.getBox().tl());
+    grid.setCenter(grid.getCenter() + tag.getBox().tl());
 
-	std::string title("decoding: " + decoding.to_string());
-	cv::namedWindow(title);
-	cv::imshow(title, canvas);
+    std::string title("decoding: " + decoding.to_string());
+    cv::namedWindow(title);
+    cv::imshow(title, canvas);
 
-	bool cont = true;
-	while (cont) {
-		const char c = cv::waitKey();
-		if (c == 'd') {
-			cv::destroyAllWindows();
-			cont = false;
-		} else if (c == 'c') {
-			cont = false;
-		}
-	}
+    bool cont = true;
+    while (cont) {
+        const char c = cv::waitKey();
+        if (c == 'd') {
+            cv::destroyAllWindows();
+            cont = false;
+        } else if (c == 'c') {
+            cont = false;
+        }
+    }
 }
 
 double Decoder::getMeanIntensity(const cv::Mat &image, const PipelineGrid::coordinates_t &coords, const cv::Point& offset) const
 {
-	size_t sum = 0;
-	for (cv::Point const& loc : coords.areaCoordinates) {
-		sum += image.at<uint8_t>(loc - offset);
-	}
-	return static_cast<double>(sum) / static_cast<double>(coords.areaCoordinates.size());
+    size_t sum = 0;
+    for (cv::Point const& loc : coords.areaCoordinates) {
+        sum += image.at<uint8_t>(loc - offset);
+    }
+    return static_cast<double>(sum) / static_cast<double>(coords.areaCoordinates.size());
 }
 
 double Decoder::getMeanDistance(const cv::Mat &image, const PipelineGrid::coordinates_t &coords, const cv::Point& offset, const double mean) const
 {
-	double sum = 0;
-	for (cv::Point const& loc : coords.areaCoordinates) {
-		sum += std::abs(image.at<uint8_t>(loc - offset) - mean);
-	}
-	return sum / static_cast<double>(coords.areaCoordinates.size());
+    double sum = 0;
+    for (cv::Point const& loc : coords.areaCoordinates) {
+        sum += std::abs(image.at<uint8_t>(loc - offset) - mean);
+    }
+    return sum / static_cast<double>(coords.areaCoordinates.size());
 }
 }
--- Decoder.h
+++ Decoder.h
@@ -15,42 +15,42 @@ typedef struct {
 
 class Decoder {
 public:
-	Decoder();
+    Decoder();
 
-	void loadSettings(decoder_settings_t&& settings);
+    void loadSettings(decoder_settings_t&& settings);
 
-	std::vector<Tag> process(std::vector<Tag>&& taglist);
+    std::vector<Tag> process(std::vector<Tag>&& taglist);
 
 private:
-	decoder_settings_t _settings;
+    decoder_settings_t _settings;
 
-	std::vector<decoding_t> getDecodings(pipeline::Tag const& tag, TagCandidate &candidate) const;
+    std::vector<decoding_t> getDecodings(pipeline::Tag const& tag, TagCandidate &candidate) const;
 
-	void visualizeDebug(pipeline::Tag const& tag, PipelineGrid &grid, const decoding_t &decoding) const;
+    void visualizeDebug(pipeline::Tag const& tag, PipelineGrid &grid, const decoding_t &decoding) const;
 
-	class mean_calculator_t {
-	public:
-		explicit mean_calculator_t(cv::Mat const& roi, const cv::Point roiOffset)
-		    : _roi(roi), _roiOffset(roiOffset), _sum(0), _pixelNum(0) {}
+    class mean_calculator_t {
+    public:
+        explicit mean_calculator_t(cv::Mat const& roi, const cv::Point roiOffset)
+            : _roi(roi), _roiOffset(roiOffset), _sum(0), _pixelNum(0) {}
 
-		inline void operator()(const cv::Point coords) {
-			if (Util::pointInBounds(_roi.get().size(), coords - _roiOffset)) {
-				const uint8_t value = _roi.get().template at<uint8_t>(coords - _roiOffset);
-				_sum += value;
-				++_pixelNum;
-			}
-		}
+        inline void operator()(const cv::Point coords) {
+            if (Util::pointInBounds(_roi.get().size(), coords - _roiOffset)) {
+                const uint8_t value = _roi.get().template at<uint8_t>(coords - _roiOffset);
+                _sum += value;
+                ++_pixelNum;
+            }
+        }
 
-		inline double getMean() const {
-			return static_cast<double>(_sum) / static_cast<double>(_pixelNum);
-		}
+        inline double getMean() const {
+            return static_cast<double>(_sum) / static_cast<double>(_pixelNum);
+        }
 
-	private:
-		std::reference_wrapper<const cv::Mat> _roi;
-		cv::Point _roiOffset;
-		size_t _sum;
-		size_t _pixelNum;
-	};
+    private:
+        std::reference_wrapper<const cv::Mat> _roi;
+        cv::Point _roiOffset;
+        size_t _sum;
+        size_t _pixelNum;
+    };
 
 
     class median_calculator_t {
@@ -165,39 +165,39 @@ private:
 
 
 
-	class distance_calculator_t {
-	public:
-		explicit distance_calculator_t(cv::Mat const& roi, const cv::Point roiOffset, const double meanBlack, const double meanWhite)
-		    : _roi(roi), _roiOffset(roiOffset), _meanBlack(meanBlack), _meanWhite(meanWhite), _distanceSumBlack(0.), _distanceSumWhite(0.), _pixelNum(0) {}
+    class distance_calculator_t {
+    public:
+        explicit distance_calculator_t(cv::Mat const& roi, const cv::Point roiOffset, const double meanBlack, const double meanWhite)
+            : _roi(roi), _roiOffset(roiOffset), _meanBlack(meanBlack), _meanWhite(meanWhite), _distanceSumBlack(0.), _distanceSumWhite(0.), _pixelNum(0) {}
 
-		inline void operator()(const cv::Point coords) {
-			if (Util::pointInBounds(_roi.get().size(), coords - _roiOffset)) {
-				const double value = static_cast<double>(_roi.get().template at<uint8_t>(coords - _roiOffset));
+        inline void operator()(const cv::Point coords) {
+            if (Util::pointInBounds(_roi.get().size(), coords - _roiOffset)) {
+                const double value = static_cast<double>(_roi.get().template at<uint8_t>(coords - _roiOffset));
 
-				_distanceSumBlack += std::abs(value - _meanBlack);
-				_distanceSumWhite += std::abs(value - _meanWhite);
+                _distanceSumBlack += std::abs(value - _meanBlack);
+                _distanceSumWhite += std::abs(value - _meanWhite);
 
-				++_pixelNum;
-			}
-		}
+                ++_pixelNum;
+            }
+        }
 
-		inline double getDistanceBlack() const {
-			return _distanceSumBlack / static_cast<double>(_pixelNum);
-		}
+        inline double getDistanceBlack() const {
+            return _distanceSumBlack / static_cast<double>(_pixelNum);
+        }
 
-		inline double getDistanceWhite() const {
-			return _distanceSumWhite / static_cast<double>(_pixelNum);
-		}
+        inline double getDistanceWhite() const {
+            return _distanceSumWhite / static_cast<double>(_pixelNum);
+        }
 
-	private:
-		std::reference_wrapper<const cv::Mat> _roi;
-		cv::Point _roiOffset;
-		double _meanBlack;
-		double _meanWhite;
-		double _distanceSumBlack;
-		double _distanceSumWhite;
-		size_t _pixelNum;
-	};
+    private:
+        std::reference_wrapper<const cv::Mat> _roi;
+        cv::Point _roiOffset;
+        double _meanBlack;
+        double _meanWhite;
+        double _distanceSumBlack;
+        double _distanceSumWhite;
+        size_t _pixelNum;
+    };
 
     // Pawel begin
 
@@ -231,12 +231,12 @@ private:
 
     // Pawel end
 
-	class dummy_functor_t {
-	public:
-		inline void operator()(const cv::Point) {}
-	};
+    class dummy_functor_t {
+    public:
+        inline void operator()(const cv::Point) {}
+    };
 
-	double getMeanIntensity(cv::Mat const& image, const PipelineGrid::coordinates_t& coords, const cv::Point& offset) const;
-	double getMeanDistance(cv::Mat const& image, const PipelineGrid::coordinates_t& coords, const cv::Point& offset, const double mean) const;
+    double getMeanIntensity(cv::Mat const& image, const PipelineGrid::coordinates_t& coords, const cv::Point& offset) const;
+    double getMeanDistance(cv::Mat const& image, const PipelineGrid::coordinates_t& coords, const cv::Point& offset, const double mean) const;
 };
 }
